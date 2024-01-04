d54f153a7f98a5f4aace195a409d8dc3d8312912
rebeca3081 <hyoeun3081@gmail.com>
2024-01-04 10:46:41 +0900

    map,set 처리후

--- BoardWeb/src/main/webapp/javascript/js/class4.js
+++ BoardWeb/src/main/webapp/javascript/js/class4.js
@@ -11,13 +11,43 @@ const friend = {
   }
 }
 friend.pets[0].friends[2] = '노냥이';
-delete friend.age; // 속성 삭제
+// delete friend.age; // 속성 삭제
 friend.ages = 20; // 속성 추가
 friend.showFriends = function () { // 메소드 추가
   console.log(this); // friend 객체를 가리킴
 }
-console.log(friend['pets'][0]['friends'][0]); // ['속성명'] 값 접근
-console.log(friend.pets[0].friends[0]); // . 접근
-console.log(friend.pets[0].friends);
+// console.log(friend['pets'][0]['friends'][0]); // ['속성명'] 값 접근
+// console.log(friend.pets[0].friends[0]); // . 접근
+// console.log(friend.pets[0].friends);
 console.log(friend);
-console.log(friend.showFriends());
+// console.log(friend.showFriends());
+
+
+// ------ 240104(목) ---------
+
+// 참조값을 복사.
+const fcopy = friend; // 동일한 객체를 가리키고 있음
+fcopy.age = 22;
+console.log(fcopy);
+
+// 객체를 복사. : 원본 객체에는 변경이 되지 않음
+const fcopy2 = Object.assign({ bloodType: 'A' }, friend); // assign(빈 객체, 복사할 대상)
+fcopy2.age = 24;
+console.log(fcopy2);
+
+// 속성추가.
+Object.defineProperty(friend, 'height', { // defineProperty(객체, 속성, );
+  get: function () { // 메소드 아님 getter : 속성의 값 불러오기
+    return this._height; // '_'제외하면 자기 자신을 무한 호출에러 (_:숨김속성의 의미)
+  },
+  set: function (value) { // setter : 속성의 값 설정하기
+    if (value <= 0) {
+      alert('error');
+    } else {
+      this._height = value;
+    }
+  }
+});
+
+friend.height = 163;
+console.log(friend.height);
--- BoardWeb/src/main/webapp/javascript/js/class5.js
+++ BoardWeb/src/main/webapp/javascript/js/class5.js
@@ -1,1 +1,69 @@
-//
+// Map, Set.
+const map = new Map();
+map.set('홍길동', 20);
+map.set('김민수', 22);
+map.set('최성우', 25);
+map.set('홍길동', 21); // 같은 key가 있으면 새로운 값으로 변경됨
+
+// 삭제
+// map.delete('최성우');
+// 전체삭제
+// map.clear();
+
+console.log(map.get('김민수'));
+
+console.log(map.keys()); // map.keys() : key 값만 컬렉션 타입으로 반환
+for (let key of map.keys()) {
+  console.log(`key는 ${key}, value는 ${map.get(key)}`);
+}
+console.log(map.values()); // map.values() : value 값만 컬렉션 타입으로 반환
+
+for (let [item, value] of map.entries()) { // map.entries() : [key, value]쌍
+  console.log(item, value);
+}
+
+// 첫번째 매개값 -> 값, 두번째 매개값 -> 키.
+map.forEach((val, key) => console.log(`key: ${key}, val: ${val}`));
+
+// set.
+const set = new Set();
+
+// 추가
+set.add('홍길동');
+set.add('김민수');
+set.add('박재희');
+set.add('홍길동'); // 중복은 제거됨
+
+set.delete('김민수');
+
+console.clear();
+console.log(set.size); // 배열의 length와 같은 역할
+
+set.forEach(name => console.log(name));
+
+const numAry = [10, 14, 12, 10]; // 중복 제거됨
+const num = new Set(numAry); // 배열 -> Set에 담기
+num.forEach(number => console.log(number));
+
+console.clear();
+map.clear();
+
+map.set('김명철', 80);
+map.set('김익수', 70);
+map.set('이명철', 80);
+map.set('이익수', 85);
+map.set('홍길동', 80);
+
+map.forEach((score, name) => console.log(name, score));
+
+// 김씨 점수의 합계와 평균
+let sum = 0;
+let cnt = 0;
+map.forEach((score, name) => {
+  if (name.indexOf('김') == 0) {
+    sum += score;
+    cnt++;
+  }
+})
+console.log(`합계는: ${sum}, 평균은: ${sum/cnt}`);
+
