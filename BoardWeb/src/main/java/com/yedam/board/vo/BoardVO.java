f20417fe2865187d26cfff05d19d819998472bb5
rebeca3081 <hyoeun3081@gmail.com>
2024-01-23 10:42:25 +0900

    24.01.23 커밋정리

--- BoardWeb/src/main/java/com/yedam/board/vo/BoardVO.java
+++ BoardWeb/src/main/java/com/yedam/board/vo/BoardVO.java
@@ -2,9 +2,15 @@ package com.yedam.board.vo;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder // .builder().boardNo(2) => 인스턴스 생성없이 바로 만들 수 있게 해주는 메소드 역할
+@NoArgsConstructor // 기본생성자 생성
+@AllArgsConstructor // 모든 argument에 대한 생성자 생성
 public class BoardVO {
 	// 필드
 	private int boardNo;
